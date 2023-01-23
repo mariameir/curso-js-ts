c12c05e89ee09992c4ee279217289c90bc1cd91d
Maria Meir Fernanda Sousa Castro <maria.meir@ti.local>
2023-01-23 10:51:13 -0400

    aulas 3 e 5

---
+++ aula-3/index.js
@@ -0,0 +1,8 @@
+console.log('maria "meir"');
+console.log("maria 'meir'");
+console.log(`"maria" 'meir'`);
+console.log(24);
+
+// exercicios
+console.log('Meu nome é "Meir". Estou aprendendo  JavaScript as', 10, 'da manhã.');
+
