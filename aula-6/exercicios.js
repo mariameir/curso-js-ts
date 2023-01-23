85f5871fd18c92671fc8d49aad25aa5760d1de2f
Maria Meir Fernanda Sousa Castro <maria.meir@ti.local>
2023-01-23 16:29:51 -0400

    aulas 6

---
+++ aula-6/exercicios.js
@@ -0,0 +1,4 @@
+// Maria Meir tem 24 anos, pesa 43 kg, tem 1.53 de altura e seu IMC é de 18.369003374
+
+const nome = "Maria Meir";
+const idade = 24
---
+++ aula-6/index.js
@@ -0,0 +1,15 @@
+//variaveis 
+let nome = "João";
+
+console.log(nome, "nasceu em 1999.");
+console.log("Em 2000",nome, "conheceu Maria.");
+console.log(nome,"casou-se com Maria em 2012.");
+console.log("Maria teve um filho com" ,nome, "em 2015.");
+console.log("O filho de", nome, "se chama Eduardo.");
+
+let nome1; //declarando variaveis 
+nome1 = "qualquer valor"; //inicializando variaveis 
+console.log(nome1);
+
+//contantes 
+// não pode ser redeclarada 
