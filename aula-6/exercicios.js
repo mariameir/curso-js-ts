f373b524783152c0925cbd44f3a6d55a991949c5
Maria Meir Fernanda Sousa Castro <maria.meir@ti.local>
2023-01-24 16:48:08 -0400

    aula8

--- aula-6/exercicios.js
+++ aula-6/exercicios.js
@@ -1,4 +1,12 @@
 // Maria Meir tem 24 anos, pesa 43 kg, tem 1.53 de altura e seu IMC é de 18.369003374
 
 const nome = "Maria Meir";
-const idade = 24
+const idade = 24;
+const peso = 43;
+const altura = 1.53;
+let imc;// peso / (altura*altura)
+imc = peso/(altura*altura);
+const ano = 2023; 
+let anoNascimento = ano - idade;
+console.log(nome,"tem", "anos,","pesa","kg, tem","de altura e seu IMC é de", imc);
+console.log(anoNascimento);
---
+++ aula-8/index.js
@@ -0,0 +1,8 @@
+// ECMAScript = padronização da linguagem
+var nome = "Meir";
+var nome = "Maria";
+console.log(nome);
+// LET N PERMITE REDECLARAÇÃO DE VALORES
+let idade = 12;
+//let idade = 24;
+console.log(idade);
