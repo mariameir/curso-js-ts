03e74d992a463c5f2798e70a7ee11067bbb09aa5
Maria Meir Fernanda Sousa Castro <maria.meir@ti.local>
2023-02-13 15:45:54 -0400

    aula 38

---
+++ aula-38/assets/js/main.js
@@ -0,0 +1,12 @@
+const paragrafos = document.querySelector('.paragrafos');
+const ps = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+
+for (let p of ps){
+
+    p.style.color = "white";
+    p.style.background = backgroundColorBody;
+    
+} 
