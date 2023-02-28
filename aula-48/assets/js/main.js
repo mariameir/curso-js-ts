26a08df577c642a7056c6f684cd3693d9ca8433b
Maria Meir <pigyarx@gmail.com>
2023-02-28 10:48:49 -0400

    aula 48 - layout do projeto

---
+++ aula-48/assets/js/main.js
@@ -0,0 +1,9 @@
+const inputTask = document.querySelector('.input-task');
+const btnTask = document.querySelector('.btn-task');
+const tarefas = document.querySelector('.tarefas');
+
+btnTask.addEventListener('click', function (event) {
+    console.log("teste")
+});
+
+
